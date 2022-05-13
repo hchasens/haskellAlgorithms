07d27ccf1235e80f58cfecebf050d0872deeaddc
Hunter Chasens <hunter.chasens18@ncf.edu>
2022-05-13 05:02:35 -0400

    Warning that sort is not finished

--- Main.hs
+++ Main.hs
@@ -2,7 +2,7 @@ import Sorts
 import Graphs
 
 main = do
-    putStrLn "Imput your choice: 'Sorts' or 'Graphs'"
+    putStrLn "Imput your choice: 'Sorts' or 'Graphs' (Sort was never finished, please stick to Graph)"
     choice <- getLine
     if choice == "Graphs" then Graphs.graphs
     else if choice == "Sorts" then Sorts.sorts
